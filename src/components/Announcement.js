1b96d0e3d0a3d3bf70030449301f8ef7e94d236f
tkmarsten <tkmarsten@gmail.com>
2022-10-24 20:41:24 -0700

    Update announcement and update sign up links

--- src/components/Announcement.js
+++ src/components/Announcement.js
@@ -5,7 +5,7 @@ const Announcement = () => {
     <Container fluid className="announcements bg-dark text-white p-4">
       <Container className="text-center">
         <h3 className="mb-4">Announcements</h3>
-        <h5>Autumn quarter begins 7 September!</h5>
+        <h5>Late autumn quarter begins 31 October!</h5>
         <p>Please register with the links below.<br />
           Masks are not required, but preferred.</p>
       </Container>
