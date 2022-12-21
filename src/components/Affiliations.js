a728a6b907b12fabfb9cfb13e9753709eb221097
tkmarsten <tkmarsten@gmail.com>
2022-12-20 19:26:05 -0800

    Add responsiveness

--- src/components/Affiliations.js
+++ src/components/Affiliations.js
@@ -5,7 +5,7 @@ const Affiliations = () => {
   return (
     <Container my='4rem'>
       <Heading as={'h3'} mb='2rem'>Affiliations</Heading>
-      <Stack direction={{ base: 1, md: 2 }} gap={10}>
+      <Stack direction={{ base: 'column', md: 'row' }} gap={10}>
         <AffImg img='auskf.jpg' url='https://www.auskf.org/home' caption='auskf' heading='All United States Kendo Federation' />
         <AffImg img='pnkf.jpg' url='https://www.pnkf.org/' caption='pnkf' heading='Pacific Northwest Kendo Federation' />
         <AffImg img='citylogo.png' url='https://federalwaykendo.org/' caption='federalway' heading='Federal Way Kendo Club' />
