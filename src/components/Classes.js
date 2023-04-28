d0f6cfa6f535b41dbcf787f0b191134ca84cd8b0
tiarnanm <tkmarsten@gmail.com>
2023-04-27 23:08:54 -0700

    Add new register and question sections

--- src/components/Classes.js
+++ src/components/Classes.js
@@ -1,15 +1,11 @@
 import BgImage from '../components/BgImage'
 import { Link as RouterLink } from 'react-router-dom'
-import { Text, Box, Link, Flex, Heading } from '@chakra-ui/react'
+import { Box, Link, Flex, Heading } from '@chakra-ui/react'
 
 const Classes = () => {
   return (
     <Box as='section'>
       <Heading fontSize={24}>Classes</Heading>
-      <Text>
-        We offer classes for all skill ranges.
-        Anyone regardless of background may start in our beginning class and progress to the advanced.
-      </Text>
       <Flex direction={{ base: 'column', md: 'row' }} align='center' justify='space-around' gap={5} mt='2rem'>
         <Box className='class-card'>
           <BgImage image='advanced.jpg' className='card-inner'>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,10 @@ import Dojo from '../components/Dojo'
 import Intro from '../components/Intro'
 import Classes from '../components/Classes'
 import Schedule from '../components/Schedule'
+import dojoData from '../data/dojo.json'
 import { Link as RouterLink } from 'react-router-dom'
-import { Box, Text, Link, Container } from '@chakra-ui/react'
+import { Box, Container, Heading, Flex, Image, Text, Link, Button, Icon, SimpleGrid } from '@chakra-ui/react'
+import { FiMessageSquare, FiInfo } from 'react-icons/fi'
 
 const Home = () => {
   return (
@@ -15,8 +17,47 @@ const Home = () => {
         <Classes />
         <Schedule />
         <Dojo />
-        <Box py='4rem'>
-          <Text>Still have questions? Check out our <Link as={RouterLink} to='/faq' color='red'>FAQ!</Link></Text>
+        <Flex as='section' direction={{ base: 'column-reverse', md: 'row' }} align='center'>
+          <Container mt='2rem'>
+            <Heading fontSize={24}>
+              Ready to start?
+            </Heading>
+            <Text my='1rem'>
+              We offer classes for all skill ranges. Anyone regardless of age, gender, or background
+              may start in our beginning class and progress to the advanced.
+            </Text>
+            <Link as={RouterLink}>
+              <Button>
+                Register now
+              </Button>
+            </Link>
+          </Container>
+          <Image src='img/join.jpg' w={250}></Image>
+        </Flex>
+        <Box as='section'>
+          <Heading fontSize={24} mb='2rem'>Still have questions?</Heading>
+          <SimpleGrid columns={{ base: 1, md: 2 }} gap={5} placeItems='center'>
+            <Box maxW='300px'>
+              <Heading fontSize={20} display='flex' alignItems='center' justifyContent='center' mb={1}>
+                <Icon as={FiInfo} boxSize={6} mr={1} />
+                <Link as={RouterLink} to='/faq'>
+                  FAQ
+                </Link>
+              </Heading>
+              <Text>
+                Check out our FAQ <Link as={RouterLink} to='/faq'><Button variant='link'>here.
+                </Button></Link> We have answered lots of questions regarding class, pricing, and some other basic parts of kendo.
+              </Text>
+            </Box>
+            <Box maxW='300px'>
+              <Heading fontSize={20} display='flex' alignItems='center' justifyContent='center' mb={1}>
+                <Icon as={FiMessageSquare} boxSize={6} mr={1} />
+                Contact
+              </Heading>
+              <Text>FAQ does not have what you need? <Link href={`mailto:` + dojoData.email}><Button variant='link'>Email
+              </Button></Link> us and we wills try our best to answer your question! Typical response time is 1-2 hours.</Text>
+            </Box>
+          </SimpleGrid>
         </Box>
       </Container>
     </Box>
