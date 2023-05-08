24b820f917d2cd9f04d1f3477f265af540b8b12b
tiarnanm <tkmarsten@gmail.com>
2023-05-08 11:57:29 -0700

    Redo About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,9 @@
-import Banner from '../components/AboutBanner'
-import Affiliations from '../components/Affiliations'
 import { Box } from '@chakra-ui/react'
 
 const About = () => {
   return (
     <Box align='center'>
-      <Banner />
-      <Affiliations />
+
     </Box>
   )
 }
