65740c9ab946d4ff0ba97333a71c94656c3a44df
tiarnanm <tkmarsten@gmail.com>
2023-05-08 12:07:48 -0700

    Add website alert for clarity

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,14 @@
 import dojoData from '../data/dojo.json'
-import { Container, Image, Heading, Icon, Link, Text, Box, Stack } from '@chakra-ui/react'
+import { Container, Image, Heading, Icon, Link, Text, Box, Stack, Alert, AlertIcon } from '@chakra-ui/react'
 import { FiMail } from 'react-icons/fi'
 
 const Footer = () => {
   return (
     <Box as='footer' bg='#252525' color='white'>
+      <Alert status='warning' color='black'>
+        <AlertIcon />
+        This website is currently under construction and some things may be unavailable at this time. Thank you for your patience.
+      </Alert>
       <Container py='2rem' centerContent>
         <Stack align='center' gap={5}>
           <Stack align='center'>
